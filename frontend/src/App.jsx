ac37eadf94efc57368a3949436de691399a1e1ed
yamartinez03 <yamartinez@frba.utn.edu.ar>
2026-08-27 19:53:21 -0300

    fix(modulo-publicaciones): corrige redireccionamientos para poner a la page de publicaciones como principal, ademas suma el router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,12 @@
-import { Button } from './components/ui/button'
+import { Routes, Route } from "react-router-dom";
+import PublicacionesPage from "./pages/publicaciones/PublicacionesPage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      
-      {/* Navbar */}
-      <nav className="bg-white border-b border-gray-200 px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <span className="text-2xl">🐾</span>
-          <h1 className="text-lg font-bold text-gray-900">Callejeritos Villa Elisa</h1>
-        </div>
-        <Button>Iniciar sesión</Button>
-      </nav>
-
-      {/* Contenido */}
-      <main className="max-w-4xl mx-auto px-6 py-10">
-        <h2 className="text-2xl font-bold text-black mb-2">Bienvenido al sistema</h2>
-        <p className="text-gray-500 mb-8">Gestión integral de animales, tránsitos y adopciones.</p>
-
-      </main>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<PublicacionesPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
